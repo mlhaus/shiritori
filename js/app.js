f782e3144fec7ea1c3742109b1450b98ecc75155
Dylan Schroeder <schrodac@gmail.com>
2018-06-25 15:19:36 -0500

    Add constructor for player, dictionary, game

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-// include the Node.js File System module
-var fs = require('fs');
-var text = fs.readFileSync('../assets/dictionary.txt');
-var textByLine = text.split('\n');
+'use strict';
 
-for(var i = 0; i < 10; i++) {
-  console.log(textByLine[i]);
+//Constructors for player, game, dictionary, settings
+function Player (name, highScore, viewStat, profilePic) {
+  this.name = name;
+  this.highScore = highScore;
+  this.viewStat = viewStat;
+  this.profilePic = profilePic;
+}
+
+function Dictionary (name, listOfWords, alphabet) {
+  this.name = name;
+  this.listOfWords = [];
+  this.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+}
+
+
+function Game (scores, wordsTyped, dictionary, time, players) {
+  this.scores = [];
+  this.wordsTyped = [];
+  this.dictionary = dictionary;
+  this.time = time;
+  this.players = ['Player1', 'Player2'];
 }
